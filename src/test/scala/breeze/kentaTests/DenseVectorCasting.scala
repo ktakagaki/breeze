7d2b241e3ac157d91688f6312d7353262640333f
ktakagaki <kentaroh.takagaki@gmail.com>
2014-02-14 13:55:34 +0100

    Dense Vector Casting, fastest is with fast loop from DV to Array

--- src/test/scala/breeze/kentaTests/DenseVectorCasting.scala
+++ src/test/scala/breeze/kentaTests/DenseVectorCasting.scala
@@ -12,25 +12,40 @@ class DenseVectorCasting extends FunSuite {
   val tempDataDouble = DenseVector.rand[Double](1000000)
 
 //  test("via map") {
-//    var dataTemp = DenseVector[Long]()
+//    var dataTemp = DenseVector[Long](tempDataDouble.length)
 //    for( count <-0 until 10000){
-//      dataTemp += tempDataDouble.map(_.toLong)
+//      dataTemp :+= tempDataDouble.map(_.toLong)
 //    }
 //  }
 
-  test("via array") {
-    var dataTemp = DenseVector[Long]()
-    for( count <-0 until 10000){
+//  test("via array") {
+//    var dataTemp = DenseVector[Long](tempDataDouble.length)
+//    for( count <-0 until 10000){
+//
+//      dataTemp :+= DenseVector( castArrayToDouble( tempDataDouble.toArray ) )
+//    }
+//  }
 
-      dataTemp += DenseVector( castArrayToDouble( tempDataDouble.toArray ) )
+  test("via array without cast") {
+    var dataTemp = DenseVector[Long](tempDataDouble.length)
+    for( count <-0 until 10000*1000){
+      dataTemp :+= castDenseVectorToDouble( tempDataDouble )
     }
   }
+
   def castArrayToDouble(array: Array[Double]): Array[Long] = {
     var tempret = Array[Long](array.length)
     for( c <- 0 until tempret.length){
       tempret(c) = array(c).toLong
     }
     tempret
+  }
+  def castDenseVectorToDouble(array: DenseVector[Double]): DenseVector[Long] = {
+    var tempret = Array[Long](array.length)
+    for( c <- 0 until tempret.length){
+      tempret(c) = array(c).toLong
+    }
+    DenseVector( tempret )
   }
 
 }
